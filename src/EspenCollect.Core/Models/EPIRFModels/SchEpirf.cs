84109462e9feec547aa5d56586fcb1536796992a
Dyesse YUMBA <yumba.nduba@gmail.com>
2020-07-19 21:23:28 +0100

    add the all EPIRF Model

---
+++ src/EspenCollect.Core/Models/EPIRFModels/SchEpirf.cs
@@ -0,0 +1,25 @@
+﻿namespace EspenCollect.Core.Models
+{
+    public class SchEpirf
+    {
+        public string SurveyType { get; set; }
+        public string IuName { get; set; }
+        public string SiteName { get; set; }
+        public string Month { get; set; }
+        public string Year { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string AgeGroup { get; set; }
+        public string DiagnosticTest { get; set; }
+        public string UrinaryNumberOfPeopleExamined { get; set; }
+        public string UrinaryNumberofPositive { get; set; }
+        public string UrinaryPercentageOfPositive { get; set; }
+        public string UrinaryPercentageHeavy { get; set; }
+        public string UrinaryPercentageLow { get; set; }
+        public string IntestinalNumberOfPeopleExamined { get; set; }
+        public string IntestinalNumberofPositive { get; set; }
+        public string IntestinalPercentageOfPositive { get; set; }
+        public string IntestinalPercentageHeavy { get; set; }
+        public string IntestinalPercentageModerate { get; set; }
+    }
+}
---
+++ src/EspenCollect.Core/Models/EPIRFModels/SthEpirf.cs
@@ -0,0 +1,35 @@
+﻿namespace EspenCollect.Core.Models
+{
+    public class SthEpirf
+    {
+        public string SurveyType { get; set; }
+        public string IuName { get; set; }
+        public string CommunityName { get; set; }
+        public string NumberOfRoundsPC { get; set; }
+        public string Month { get; set; }
+        public int? Year { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public string AgeGroupSurveyed { get; set; }
+        public string DiagnosticTest { get; set; }
+        public int? AscarisNumberOfPeopleExamined { get; set; }
+        public int? AscarisNumberOfPeoplePositive { get; set; }
+        public float? AscarisPercentagePositive { get; set; }
+        public float? AscarisPercentageHeavy { get; set; }
+        public float? AscarisPercentageModerate { get; set; }
+        public int? HookwormNumberOfPeopleExamined { get; set; }
+        public int? HookwormNumberOfPeoplePositive { get; set; }
+        public float? HookwormPercentagePositive { get; set; }
+        public float? HookwormPercentageHeavy { get; set; }
+        public float? HookwormPercentageModerate { get; set; }
+        public int? TrichurisNumberOfPeopleExamined { get; set; }
+        public int? TrichurisNumberOfPeoplePositive { get; set; }
+        public float? TrichurisPercentagePositive { get; set; }
+        public float? TrichurisPercentageHeavy { get; set; }
+        public float? TrichurisPercentageModerate { get; set; }
+        public int? SthExamined { get; set; }
+        public int? SthPositive { get; set; }
+        public float? SthPercentagePositve { get; set; }
+
+    }
+}
